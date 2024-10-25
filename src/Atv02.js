dc08d28228732f7204fc850d35db4b9d40f6c977
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-10-25 10:11:32 -0300

    criação

---
+++ src/Atv02.js
@@ -0,0 +1,16 @@
+import Relogio from "./Relogio";
+import Letreiro from "./Letreiro";
+import {Link} from "react-router-dom";
+
+
+function Atv02(){
+    return (
+        <>
+            <h2>Atividade 02</h2>
+            <Letreiro/>
+            <Relogio/>
+            <Link to="/">Voltar</Link>
+        </>
+    );
+}
+export default Atv02;
